86845ced34481cbeadf0470af74919804d28b179
Thomas Haslam <thomas.haslam@softwire.com>
2020-07-08 10:34:27 +0100

    names make more sense

--- app/src/main/java/com/example/gildedrose/GildedRose.kt
+++ app/src/main/java/com/example/gildedrose/GildedRose.kt
@@ -8,32 +8,36 @@ class GildedRose(var items: Array<Item>) {
     }
     fun getQualityDecay(i:Item):Int{
         if (i.name.contains("Aged Brie")){
-            return 1
+            return -1
         }else if (i.name.contains("Backstage passes")){
             return when{
-                (i.sellIn<=0)-> -i.quality //expired ticket, quality>=0 so doubling won't affect this
-                (i.sellIn<5)-> 3
-                (i.sellIn<10)-> 2
-                else-> 1
+                (i.sellIn<=0)-> i.quality //expired ticket, quality>=0 so doubling won't affect this
+                (i.sellIn<5)-> -3
+                (i.sellIn<10)-> -2
+                else-> -1
             }
         } else {
-            return -1
+            return 1
         }
     }
     fun updateQuality(i:Item){
         if (i.name.contains("Sulfuras")){
             return
         }
-        var qDecay=getQualityDecay(i)
+        var qualityDecay=getQualityDecay(i)
         if (i.sellIn<=0){
-            qDecay*=2
+            qualityDecay*=2
         }
         if (i.name.contains("Conjured")){
-            qDecay*=2
+            qualityDecay*=2
         }
-        i.quality+=qDecay
+        i.quality-=qualityDecay
         //clamp quality
-        i.quality=when{(i.quality<0)->0;(i.quality>50)->50;else->i.quality}
+        i.quality=when{
+            (i.quality<0)->0
+            (i.quality>50)->50
+            else->i.quality
+        }
     }
     fun updateItems() {
         items.forEach{
