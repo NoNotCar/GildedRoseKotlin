081185c79fbdb7db18fbcdc3721827326aa48c6f
samdix <samueljosephdixon@gmail.com>
2020-07-08 09:50:28 +0100

    Split the tests down into smaller, more descriptive tests

--- app/src/test/java/com/example/gildedrose/GildedRoseTest.kt
+++ app/src/test/java/com/example/gildedrose/GildedRoseTest.kt
@@ -7,37 +7,51 @@ class GildedRoseTest {
     fun single_item_setup(i:Item):GildedRose{
         return GildedRose(arrayOf(i))
     }
-    @Test fun basic_item() {
+    @Test fun basic_items_quality_falls_once_per_day() {
+        val app = single_item_setup(Item("spam", 10, 5))
+        app.updateQuality()
+        assertEquals("spam", app.items[0].name)
+        assertEquals(4,app.items[0].quality)
+
+    }
+
+    @Test fun basic_items_sellin_falls_once_per_day() {
         val app = single_item_setup(Item("spam", 10, 5))
         app.updateQuality()
         assertEquals("spam", app.items[0].name)
         assertEquals(9, app.items[0].sellIn)
-        assertEquals(4,app.items[0].quality)
 
     }
     @Test fun expired_items_degrade_twice_as_fast(){
-        //checks that expired items degrade twice as fast
         val app = single_item_setup(Item("dodgy milk", 0, 5))
         app.updateQuality()
         assertEquals(3,app.items[0].quality)
     }
-    @Test fun quality_limits(){
-        //checks that quality cannot exceed 50, and cannot go below 0
-        val app = single_item_setup(Item("pile of rust", 0, 0))
-        app.updateQuality()
-        assertEquals(0,app.items[0].quality)
+    @Test fun quality_doesnt_exceed_50(){
         val app2=single_item_setup(Item("Aged Brie",10,50))
         app2.updateQuality()
         assertEquals(50,app2.items[0].quality)
     }
-    @Test fun brie_increases(){
+
+    @Test fun quality_is_never_negative(){
+        val app = single_item_setup(Item("pile of rust", 0, 0))
+        app.updateQuality()
+        assertEquals(0,app.items[0].quality)
+
+        //Check this is still true for a "worst case" conjured, expired item
+        val app1 = single_item_setup(Item("Conjured something", 0, 1))
+        app1.updateQuality()
+        assertEquals(0,app1.items[0].quality)
+    }
+
+    @Test fun brie_quality_increases_with_time(){
         //checks that the value of brie increases with time
         val app = single_item_setup(Item("Aged Brie",10,30))
         app.updateQuality()
         assertEquals(31,app.items[0].quality)
     }
 
-    @Test fun Sulfuras_is_legendary(){
+    @Test fun Sulfuras_is_legendary_and_so_does_not_degrade(){
         //checks that Sellin (the days to the sell-by-date) do not decrease for "Sulfuras, Hand of Ragnaros"
         //also checks that the value of the item does not reduce.
         val app = single_item_setup(Item("Sulfuras, Hand of Ragnaros",10,30))
@@ -65,13 +79,11 @@ class GildedRoseTest {
         assertEquals(0,app.items[0].quality)
     }
 
-    @Test fun conjured_expired(){
-        //Checks conjured, expired items degrade at a rate of -4 per day, and that they do not go negative.
+    @Test fun conjured_expired_degrade_at_4_per_day(){
         val app = single_item_setup(Item("Conjured spam", 1, 7))
         app.updateQuality()
         assertEquals(3, app.items[0].quality)
-        app.updateQuality()
-        assertEquals(0, app.items[0].quality)
+
 
     }
 
@@ -95,7 +107,7 @@ class GildedRoseTest {
         assertEquals(11,app.items[0].quality)
     }
 
-    @Test fun Conjured_Sulfuras(){
+    @Test fun Conjured_Sulfuras_doesnt_change_in_quality(){
         //Checks that conjured sulfuras doesn't change in quality
         val app = single_item_setup(Item("Conjured Sulfuras", 11, 5))
         app.updateQuality()
